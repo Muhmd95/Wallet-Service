c14ab71dc1cab0af423ebb5b2ca92844c655ceae
Mohamed Osama <mosama@axisapp.com>
2026-07-16 15:32:14 +0300

    feat: define wallet database schema and configure go workspace
    Initialize multi-module go workspace, add MongoDB driver dependencies, and define the Wallet BSON struct for the domain layer.

--- svc-wallet/internal/wallet/model.go
+++ svc-wallet/internal/wallet/model.go
@@ -1,1 +1,24 @@
 package wallet
+
+// the schema for the wallet collection in the database
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Wallet struct {
+	// main attributes
+	// used bson and not bson and json to not expose it
+	// to the outside world as the DTO will be used for that purpose
+	ID           primitive.ObjectID  `bson:"_id,omitempty"`
+	OwnerName    string              `bson:"owner_name"`
+	Balance      int64               `bson:"balance"`
+	CurrencyCode string              `bson:"currency_code"`
+	FamilyID     *primitive.ObjectID `bson:"family_id,omitempty"` // a pointer because it can be null if the wallet is not part of a family
+
+	// time stamps
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+}
